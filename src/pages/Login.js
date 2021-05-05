22228a0117f2cc0782627da608ba0cb0ff957e26
Luiz Reina <luizreeina@gmail.com>
2021-05-05 16:36:52 -0300

    update readme

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -42,7 +42,7 @@ class Login extends React.Component {
     const haveUser = userList
       .some((user) => user.nome === nome && user.senha === senha);
     if (haveUser) {
-      // this.getData();
+      this.getData();
       login(nome);
       return;
     }
